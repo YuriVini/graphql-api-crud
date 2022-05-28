8829bba4996337534080421d057ab18bbcdfa48a
Yuri Vinicius <yuri.ybgt.yfo@gmail.com>
2022-05-27 21:33:22 -0300

    Subscription

--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,4 +1,5 @@
 import User from "../../../models/User";
+import { USER_ADDED } from "./channels";
 
 export default {
   User: {
@@ -9,9 +10,22 @@ export default {
     user: async (_, { id }) => await User.findById(id),
   },
   Mutation: {
-    createUser: async (_, { data }) => await User.create(data),
+    createUser: async (_, { data }, { pubsub }) => {
+      const user = await User.create(data);
+
+      pubsub.publish(USER_ADDED, {
+        userAdded: user,
+      });
+
+      return user;
+    },
     updateUser: async (_, { id, data }) =>
       await User.findOneAndUpdate(id, data, { new: true }),
     deleteUser: async (_, { id }) => !!(await User.findByIdAndDelete(id)),
+  },
+  Subscription: {
+    userAdded: {
+      subscribe: (obj, args, { pubsub }) => pubsub.asyncIterator(USER_ADDED),
+    },
   },
 };
